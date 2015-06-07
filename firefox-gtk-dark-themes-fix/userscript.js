0200852adf51aa2458fcd38f7f3f1bc54083699a
darkalemanbr <darkalemanbr@users.noreply.github.com>
2015-06-07 10:16:08 -0300

    Create file with basic metadata

---
+++ firefox-gtk-dark-themes-fix/userscript.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name Firefox GTK+ dark themes fix
+// @version 0.0
+// @description Resets colors in all pages to match those of a bright theme and, hopefully, fix annoying inconsistencies caused by using darker GTK+ themes with Firefox.
+// @namespace https://github.com/darkalemanbr/userscripts
+// @include *
+// ==/UserScript==
