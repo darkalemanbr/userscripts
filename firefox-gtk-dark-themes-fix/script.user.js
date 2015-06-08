ddee40897ba6a3ee2a4fde8fd86e5e8fef36130d
darkalemanbr <darkalemanbr@users.noreply.github.com>
2015-06-07 23:06:07 -0300

    Use root element instead of global in CSS

--- firefox-gtk-dark-themes-fix/script.user.js
+++ firefox-gtk-dark-themes-fix/script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Firefox GTK+ dark themes fix
-// @version 0.3
+// @version 0.4
 // @description Resets colors in all pages to match those of a bright theme and, hopefully, fix annoying inconsistencies caused by using darker GTK+ themes with Firefox.
 // @namespace https://github.com/darkalemanbr/userscripts
 // @include *
@@ -10,7 +10,7 @@
 // ==/UserScript==
 
 var css = '' +
-'* { color: #000; background: #FFF; }'
+':root { color: #000; background: #FFF; }'
 ;
 
 $('link[rel=stylesheet]:first-of-type, style:first-of-type').before('<style type="text/css">' + css + '"</style>');
