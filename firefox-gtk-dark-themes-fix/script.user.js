1e81aaf0be6634258539b2f68ec2c1ad5459e520
darkalemanbr <darkalemanbr@users.noreply.github.com>
2015-06-07 23:02:24 -0300

    Update script.user.js

--- firefox-gtk-dark-themes-fix/script.user.js
+++ firefox-gtk-dark-themes-fix/script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Firefox GTK+ dark themes fix
-// @version 0.2
+// @version 0.3
 // @description Resets colors in all pages to match those of a bright theme and, hopefully, fix annoying inconsistencies caused by using darker GTK+ themes with Firefox.
 // @namespace https://github.com/darkalemanbr/userscripts
 // @include *
@@ -9,8 +9,8 @@
 // @require https://cdnjs.cloudflare.com/ajax/libs/zepto/1.1.6/zepto.min.js
 // ==/UserScript==
 
-var cssUrl = 'https://raw.githubusercontent.com/darkalemanbr/userscripts/master/firefox-gtk-dark-themes-fix/style.css?v=0.2'; // match version here to prevent caching
+var css = '' +
+'* { color: #000; background: #000; }'
+;
 
-$.get(cssUrl, function (data) {
-  $('link[rel=stylesheet]:first-of-type, style:first-of-type').before('<style type="text/css">' + data + '"</style>');
-});
+$('link[rel=stylesheet]:first-of-type, style:first-of-type').before('<style type="text/css">' + css + '"</style>');
